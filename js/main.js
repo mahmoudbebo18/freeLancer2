90dcdbef4c199f2100da4007b0436a54a17fbcbe
mahmoudbebo18 <mahmoudelkhateb18@gmail.com>
2021-12-31 18:08:05 +0200

    adding new pages

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+    $('.experiance ul li').click(function(){
+        $('.experiance ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    }),
+    
+    $('.certificate ul li').click(function(){
+        $('.certificate ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    });
+
+    $('.major ul li').click(function(){
+        $('.major ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+
+
+})
