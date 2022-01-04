6a35619a4471d874a35bdd4957e1d150acacb3ae
mahmoudbebo18 <mahmoudelkhateb18@gmail.com>
2022-01-04 02:30:32 +0200

    finish project pages

--- js/main.js
+++ js/main.js
@@ -19,5 +19,41 @@ $(document).ready(function(){
         
     })
 
+    $('.first-row .number li').click(function(){
+        $('.first-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    });
+    $('.sec-row .number li').click(function(){
+        $('.sec-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.third-row .number li').click(function(){
+        $('.third-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $(' .fourth-row .number li').click(function(){
+        $('.fourth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.fifth-row  .number li').click(function(){
+        $('.fifth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.sixth-row .number li').click(function(){
+        $('.sixth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.seventh-row .number li').click(function(){
+        $('.seventh-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+
 
 })
